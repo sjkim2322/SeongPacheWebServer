fe392f761d3ffd538826585d281a253efbf58b95
KIMSEONGJIN <sjkim2322@naver.com>
2017-08-04 14:36:19 +0900

    Step - 00

---
+++ src/main/java/org/seongjin/SeongPache/App.java
@@ -0,0 +1,13 @@
+package org.seongjin.SeongPache;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
